76cc4a54ab607e418cd13eb536f45fb0461eaab7
Aniruddha Shastri <aniruddha.shastri@gmail.com>
2014-11-18 04:10:05 -0500

    Added EMR

--- src/augur/org/Main.java
+++ src/augur/org/Main.java
@@ -1,16 +1,17 @@
 package augur.org;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduce;
 import com.amazonaws.services.elasticmapreduce.AmazonElasticMapReduceClient;
-import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsRequest;
-import com.amazonaws.services.elasticmapreduce.model.AddJobFlowStepsResult;
 import com.amazonaws.services.elasticmapreduce.model.HadoopJarStepConfig;
+import com.amazonaws.services.elasticmapreduce.model.JobFlowInstancesConfig;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowRequest;
+import com.amazonaws.services.elasticmapreduce.model.RunJobFlowResult;
 import com.amazonaws.services.elasticmapreduce.model.StepConfig;
 import com.amazonaws.services.elasticmapreduce.util.StepFactory;
 
@@ -41,99 +42,90 @@ public class Main {
 			 * data and load it in Hive DB.
 			 */
 			// 1, 2 later
-			
+
 			List<String> movieList = new ArrayList<String>();
-			
+
 			movieList.add("American Hustle");
-			
+
 			// 3
 			/*
-			String cmd = "java -jar Youtube.jar";
-			for(String movie : movieList) {
-				cmd = cmd + " " + movie;
-			}
-			try {
-				String line;
-				Process p = Runtime.getRuntime().exec(cmd);
-				BufferedReader bri = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-				while ((line = bri.readLine()) != null) {
-			        System.out.println(line);
-			      }
-			      bri.close();
-			      p.waitFor();
-			} catch (IOException e) {
-				System.out.println("Error in YT: " + e.getMessage());
-				return;
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			cmd = "java -jar RottenTomatoes.jar";
-			for(String movie : movieList) {
-				cmd = cmd + " " + movie;
-			}
-			try {
-				Process p = Runtime.getRuntime().exec(cmd);
-				p.waitFor();
-			} catch (IOException e) {
-				System.out.println("Error in RT: " + e.getMessage());
-				return;
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			cmd = "java -jar Twitter.jar";
-			for(String movie : movieList) {
-				cmd = cmd + " " + movie;
-			}
-			try {
-				Process p = Runtime.getRuntime().exec(cmd);
-				p.waitFor();
-			} catch (IOException e) {
-				System.out.println("Error in Twitter: " + e.getMessage());
-				return;
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
-			
+			 * String cmd = "java -jar Youtube.jar"; for(String movie :
+			 * movieList) { cmd = cmd + " " + movie; } try { String line;
+			 * Process p = Runtime.getRuntime().exec(cmd); BufferedReader bri =
+			 * new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			 * while ((line = bri.readLine()) != null) {
+			 * System.out.println(line); } bri.close(); p.waitFor(); } catch
+			 * (IOException e) { System.out.println("Error in YT: " +
+			 * e.getMessage()); return; } catch (InterruptedException e) { //
+			 * TODO Auto-generated catch block e.printStackTrace(); } cmd =
+			 * "java -jar RottenTomatoes.jar"; for(String movie : movieList) {
+			 * cmd = cmd + " " + movie; } try { Process p =
+			 * Runtime.getRuntime().exec(cmd); p.waitFor(); } catch (IOException
+			 * e) { System.out.println("Error in RT: " + e.getMessage());
+			 * return; } catch (InterruptedException e) { // TODO Auto-generated
+			 * catch block e.printStackTrace(); } cmd = "java -jar Twitter.jar";
+			 * for(String movie : movieList) { cmd = cmd + " " + movie; } try {
+			 * Process p = Runtime.getRuntime().exec(cmd); p.waitFor(); } catch
+			 * (IOException e) { System.out.println("Error in Twitter: " +
+			 * e.getMessage()); return; } catch (InterruptedException e) { //
+			 * TODO Auto-generated catch block e.printStackTrace(); }
+			 */
+
 			// 4
 			AWSCredentials credentials = null;
 			try {
-			    credentials = new PropertiesCredentials(
-			        Main.class.getResourceAsStream("AwsCredentials.properties"));
-			} catch (IOException e1) {
-			    System.out.println("Credentials were not properly entered into AwsCredentials.properties.");
-			    System.out.println(e1.getMessage());
-			    return;
+				credentials = new ProfileCredentialsProvider().getCredentials();
+			} catch (Exception e) {
+				throw new AmazonClientException(
+						"Cannot load credentials from the file", e);
 			}
-			
-			AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(credentials);
 
-		    // predefined steps. See StepFactory for list of predefined steps
-		    StepConfig hive = new StepConfig("Hive", new StepFactory().newInstallHiveStep());
+			AmazonElasticMapReduce client = new AmazonElasticMapReduceClient(
+					credentials);
 
-		    // A custom step
-		    HadoopJarStepConfig hadoopConfig1 = new HadoopJarStepConfig()
-		        .withJar("s3://augurframework/bin/AugurMapreduce.jar")
-		        .withArgs("s3://augurframework/mapreduceInput", "s3://augurframework/mapreduceOutput"); // optional list of arguments
-		    StepConfig customStep = new StepConfig("Step1", hadoopConfig1);
+			// predefined steps. See StepFactory for list of predefined steps
+			StepConfig hive = new StepConfig("Hive",
+					new StepFactory().newInstallHiveStep());
 
-		    AddJobFlowStepsResult result = client.addJobFlowSteps(new AddJobFlowStepsRequest()
-		        .withJobFlowId("j-1HTE8WKS7SODR")
-		        .withSteps(hive, customStep));
-		    System.out.println(result.getStepIds());
-			
-			
+			StepConfig debug = new StepConfig("Debug",
+					new StepFactory().newEnableDebuggingStep());
+
+			// A custom step
+			HadoopJarStepConfig hadoopConfig1 = new HadoopJarStepConfig()
+					.withJar("s3://augurframework/bin/AugurMapreduce.jar")
+					.withArgs("s3://augurframework/mapreduceInput",
+							"s3://augurframework/outputs/out7"); // optional
+																	// list of
+																	// arguments
+			StepConfig customStep = new StepConfig("augurTrain", hadoopConfig1);
+
+			RunJobFlowResult result = client.runJobFlow(new RunJobFlowRequest()
+					.withSteps(debug, hive, customStep)
+					.withAmiVersion("3.3.0")
+					.withInstances(
+							new JobFlowInstancesConfig().withInstanceCount(3)
+									.withMasterInstanceType("m1.medium")
+									.withSlaveInstanceType("m1.medium")
+									.withKeepJobFlowAliveWhenNoSteps(false)
+									.withEc2KeyName("KeyPair")
+									.withHadoopVersion("2.4.0"))
+					.withName("augurmr").withLogUri("s3://augurframework/log/")
+					.withServiceRole("EMR_DefaultRole")
+					.withJobFlowRole("EMR_EC2_DefaultRole"));
+			// AddJobFlowStepsResult result = client.addJobFlowSteps(new
+			// AddJobFlowStepsRequest()
+			// .withJobFlowId()
+			// .withSteps(hive, customStep));
+
+			System.out.println(result.getJobFlowId());
 		} else if (args[0].equals("p")) {
 			if (args.length > 3) {
 				System.out
 						.println("Usage: \"<exec> t\" or \"<exec> p <moviename> [new]\"");
 				return;
 			}
-			String movieName = args[1];
-			boolean rePredict = args.length == 3;
+			// String movieName = args[1];
+			// boolean rePredict = args.length == 3;
 			// Predict mode
 			/*
 			 * Predict the movie box office collections
